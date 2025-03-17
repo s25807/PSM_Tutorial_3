271c04ee90e45230f667e7f5fa55b17dfb66c5d3
Michał Gromann <s25807@pjwstk.edu.pl>
2025-03-17 13:43:26 +0100

    Initial Project

---
+++ Tutorial_3/Tutorial_3/src/Containers.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tut3
+{
+    public abstract class Container
+    {
+        static protected int totalNContainers = 0;
+        protected string serialNumber;
+        private double cargoWeight;
+        private double heightCM;
+        private double tareWeightKG;
+        private double depthCM;
+        private double maximumPayloadKG;
+
+        public Container(double cargoWeightInput, double heightCMInput, double tareWeightKGInput, double depthCMInput, double maximumPayloadKGInput)
+        {
+            this.cargoWeight = cargoWeightInput;
+            this.heightCM = heightCMInput;
+            this.tareWeightKG = tareWeightKGInput;
+            this.depthCM = depthCMInput;
+            this.maximumPayloadKG = maximumPayloadKGInput;
+        }
+
+        protected abstract void SetSerialNumber();
+        public override String ToString()
+        {
+            return this.serialNumber;
+        }
+    }
+
+    public class LiquidContainer : Container
+    {
+        public LiquidContainer(double cargoWeightInput, double heightCMInput, double tareWeightKGInput, double depthCMInput, double maximumPayloadKGInput) : base(cargoWeightInput, heightCMInput, tareWeightKGInput, depthCMInput, maximumPayloadKGInput)
+        {
+            SetSerialNumber();
+        }
+
+
+        protected override void SetSerialNumber()
+        {
+            ++totalNContainers;
+        }
+        public override String ToString()
+        {
+            Console.WriteLine(this.serialNumber);
+            return this.serialNumber;
+        }
+    }
+}
---
+++ Tutorial_3/Tutorial_3/src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class ContainerApp
+{
+    public static void Main(String[] args)
+    {
+        Tut3.LiquidContainer myContainer = new Tut3.LiquidContainer(500, 400, 200, 800, 1000);
+
+        Console.WriteLine("My Container: " + myContainer);
+        
+    }
+}
